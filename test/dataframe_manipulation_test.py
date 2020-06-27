40dab39dc342dc187dc625af36a3fd00c7eed540
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2020-06-27 13:46:02 -0400

    Added pickled dataframes
    - Added pickled dataframes for testing purposes
    - Removed json files

--- test/dataframe_manipulation_test.py
+++ test/dataframe_manipulation_test.py
@@ -6,8 +6,8 @@ from src.dataframe_manipulation import AnalysisSuite
 
 
 class DataframeManipulationTest(unittest.TestCase):
-    movie_df = pd.read_json("movie_dataframe_testdata.json")
-    imdb_df = pd.read_json("imdb_dataframe_testdata.json")
+    movie_df = pd.read_pickle("movie_dataframe.pickle")
+    imdb_df = pd.read_pickle("imdb_dataframe.pickle")
     analysis_suite = AnalysisSuite(movie_df, imdb_df)
 
     def assertDataframeEqual(self, a, b, msg=None):
@@ -22,21 +22,16 @@ class DataframeManipulationTest(unittest.TestCase):
     def test_get_ratings_range(self):
         selected_movie_df, selected_imdb_df = self.analysis_suite.get_ratings_range(1.0, 2.0)
         msg = f"Assertion error raised in {self.test_get_ratings_range.__name__}"
-        self.assertDataframeEqual(selected_movie_df, pd.read_json("movie_range_dataframe_testdata.json"), msg)
-        self.assertDataframeEqual(selected_imdb_df, pd.read_json("imdb_range_dataframe_testdata.json"), msg)
+        self.assertDataframeEqual(selected_movie_df, pd.read_pickle("movie_range_dataframe.pickle"), msg)
+        self.assertDataframeEqual(selected_imdb_df, pd.read_pickle("imdb_range_dataframe.pickle"), msg)
 
     def test_select_date_range(self):
-        start_date = datetime.datetime(2020, 4, 1)
-        end_date = datetime.datetime(2020, 4, 30)
-        self.analysis_suite.movie_df.watchdate = pd.to_datetime(self.analysis_suite.movie_df.watchdate, unit='ms')
+        start_date = datetime.datetime(2020, 6, 1)
+        end_date = datetime.datetime(2020, 6, 30)
         selected_movie_df, selected_imdb_df = self.analysis_suite.select_date_range(start_date, end_date)
         msg = f"Assertion error raised in {self.test_select_date_range.__name__}"
-        test_movie_df = pd.read_json("movie_daterange_dataframe_testdata.json")
-        test_imdb_df = pd.read_json("imdb_daterange_dataframe_testdata.json")
-        test_movie_df.watchdate = pd.to_datetime(test_movie_df.watchdate, unit="ms")
-        test_imdb_df = test_imdb_df.astype({"votes": "float64"})
-        self.assertDataframeEqual(selected_movie_df, test_movie_df, msg)
-        self.assertDataframeEqual(selected_imdb_df, test_imdb_df, msg)
+        self.assertDataframeEqual(selected_movie_df, pd.read_pickle("movie_daterange_dataframe.pickle"), msg)
+        self.assertDataframeEqual(selected_imdb_df, pd.read_pickle("imdb_daterange_dataframe.pickle"), msg)
 
 
 if __name__ == '__main__':
