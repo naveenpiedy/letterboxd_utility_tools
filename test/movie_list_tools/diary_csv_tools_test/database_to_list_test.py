bf2badd9f7c55ee25f9e8e059ead823c4fec7353
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2020-07-18 11:59:35 -0400

    Fixing Travis part 3

--- test/movie_list_tools/diary_csv_tools_test/database_to_list_test.py
+++ test/movie_list_tools/diary_csv_tools_test/database_to_list_test.py
@@ -2,6 +2,9 @@ import json
 import os
 import unittest
 
+import pytest
+
+from src.database_pipeline_tools.letterboxd_rss_parser import LetterBoxdRss
 from src.movie_list_tools.dataclass_helpers import ListMovieMetadata
 from src.movie_list_tools_refactored.database_list_tools.database_to_list import DatabaseToList
 from datetime import date
@@ -9,10 +12,19 @@ from datetime import date
 path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "test_csvs")
 metadata = ListMovieMetadata(date=date(year=2020, month=7, day=4), name="generated_list", url="")
 
+rss_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "letterboxd_rss_feed_test.xml")
 
+
+@pytest.fixture(scope='function')
+def dataset():
+    obj = LetterBoxdRss(feed_url=rss_path)
+    obj.feed_db_pipeline()
+
+
+@pytest.mark.usefixtures("dataset")
 class DatabaseToListTest(unittest.TestCase):
 
-    def test_generate_normal_list(dataset):
+    def test_generate_normal_list(self):
         data = {
             "my_rating": {
                 "lower": 3,
@@ -35,9 +47,9 @@ class DatabaseToListTest(unittest.TestCase):
         english_movies = ['Moonrise Kingdom', 'Hotel Chevalier', 'The Life Aquatic with Steve Zissou']
         tamil_movies = ['Alaipayuthey', 'Mouna Raagam']
 
-        dataset.assertEqual(movie_names[:3], english_movies)
-        dataset.assertEqual(movie_names[3:], tamil_movies)
+        self.assertEqual(movie_names[:3], english_movies)
+        self.assertEqual(movie_names[3:], tamil_movies)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
