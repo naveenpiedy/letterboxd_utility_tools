dffa01ea0a091d0155e298b842ba3333a975c2b6
Naveen Piedy <nav.bugs.thepoet@gmail.com>
2020-08-01 20:52:30 -0400

    [skip travis] Added changeable border colors

--- src/card_maker/card_maker.py
+++ src/card_maker/card_maker.py
@@ -1,4 +1,5 @@
 import json
+import shutil
 
 import requests
 from jinja2 import Environment, FileSystemLoader
@@ -6,8 +7,9 @@ from jinja2 import Environment, FileSystemLoader
 from src.card_maker.get_poster import get_image_url
 from src.database_pipeline_tools.database_query_tool import DatabaseQueryTool
 from os.path import join, dirname
-from os import environ
+from os import environ, remove
 from dotenv import load_dotenv
+from colorthief import ColorThief
 
 dotenv_path = join(dirname(dirname(dirname(__file__))), '.env')
 load_dotenv(dotenv_path)
@@ -35,7 +37,7 @@ class CardMaker:
         query_results = next(iter(query_results.values()))
         imdb_info = query_results.get('imdb_info')
         movie = {'imgsrc': img_src, 'title': query_results.get('movie_title'),
-                 'director': query_results.get('director'),
+                 'director': ', '.join(person for person in imdb_info.get('director')),
                  'cast': ', '.join(person for person in imdb_info.get('cast')[:5]),
                  'my_rating': query_results.get('my_rating'),
                  'genres': ', '.join(genre for genre in imdb_info.get('genres')),
@@ -67,17 +69,29 @@ class CardMaker:
             if item.get('type') == 'Trailer' and item.get('site') == 'YouTube' and item.get('size') in (1080, 720):
                 return item.get('key')
 
+    @staticmethod
+    def get_color(poster_path):
+        response = requests.get(poster_path, stream=True)
+        with open('img.png', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+        color_thief = ColorThief("img.png")
+        # get the dominant color
+        dominant_color = color_thief.get_color(quality=1)
+        remove("img.png")
+        return '#%02x%02x%02x' % dominant_color
+
     def make_card_html(self):
         query_results = json.loads(self.get_content())
         poster_link, trailer_link = self.get_media()
         content_dict = self.parse_content(query_results=query_results, img_src=poster_link, trailer_link=trailer_link)
-
+        dominant_color = self.get_color(poster_link)
         file_loader = FileSystemLoader('templates')
         env = Environment(loader=file_loader)
-        template = env.get_template('cards.html')
-        output = template.render(movie=content_dict)
+        template = env.get_template('body.html')
+        output = template.render(movie=content_dict, dom_color=dominant_color)
         print(output)
 
 
 if __name__ == '__main__':
-    CardMaker(imdb_id='1748122').make_card_html()
+    CardMaker(imdb_id='0265666').make_card_html()
